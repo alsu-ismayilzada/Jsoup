18dcf1b9f56a9f755852252ebd8138502af2670a
Alsu Ismayilzade <131715293+alsu-ismayilzada@users.noreply.github.com>
2024-04-26 17:05:15 +0400

    updating

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,20 +2,44 @@ package org.example;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class Main {
+    private static int currentPage = 1;
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
         try {
-            Document doc = Jsoup.connect("https://turbo.az/autos/7970206-porsche-taycan-gts").get();
-            System.out.println("get request");
-            String brand = doc.select("h1.product-title ").text();
-            System.out.println("set data");
-            System.out.println(brand);
+            for(int i=currentPage; i<currentPage + 5; i++) {
+
+                Document doc = Jsoup.connect("https://turbo.az/autos/?page="+i).get();
+                Elements adLinks = doc.select(".products-i__link");
+
+                for (Element adLink : adLinks) {
+                    String adUrl = adLink.attr("abs:href");
+                    Document doc1 = Jsoup.connect(adUrl).get();
+                    String name = doc1.select("h1.product-title ").text();
+                    System.out.println("");
+                    System.out.println(name);
+
+                    Elements divs = doc1.select(".product-properties__column");
+                    for(Element div: divs){
+                        Elements elements = div.select(".product-properties__i ");
+                            String price = doc1.select(".product-price__i.product-price__i--bold").text();
+                            System.out.println("Price : "+price);
+                        for (Element element : elements) {
+                            String elementLabel = element.select("label").text();
+                            String elementSpan = element.select("span").first().text();
+                            System.out.println(elementLabel + " : " + elementSpan);
+                        }
+                    }
+
+                }
+
+            }
+            currentPage += 5;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
