00c993889313d0a9059eebf0b38a365170ca6100
Alsu Ismayilzade <131715293+alsu-ismayilzada@users.noreply.github.com>
2024-04-30 23:25:10 +0400

    feat: Update file writing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,137 +1,169 @@
 package org.example;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+
+import java.io.*;
+import java.util.Properties;
 
 public class Main {
-    private static int currentPage = 1;
-
+    private static final String CURRENT_PAGE_FILE = "current_page.properties";
+    private static final String CURRENT_PAGE = "current_page";
     public static void main(String[] args) {
-        Workbook wb = new HSSFWorkbook();
-        try (OutputStream os = new FileOutputStream("autos1.xls")) {
-            Sheet sheet = wb.createSheet("Autos");
+
+        Workbook wb = null;
+        File file = new File("autos.xls");
+
+        if(file.exists()){
+            try(FileInputStream inputStream = new FileInputStream(file)){
+                wb = WorkbookFactory.create(inputStream);
+            }catch (IOException e){
+                System.out.println(e);
+            }
+        }else{
+            wb = new HSSFWorkbook();
+        }
+
+        try {
+
+        Sheet sheet = wb.getSheet("Autos");
+        if (sheet == null) {
+
+            sheet = wb.createSheet("Autos");
+
             Row header = sheet.createRow(0);
             header.createCell(0).setCellValue("Price");
+            header.createCell(1).setCellValue("Region");
+            header.createCell(2).setCellValue("Brand");
+            header.createCell(3).setCellValue("Model");
+            header.createCell(4).setCellValue("Graduation Year");
+            header.createCell(5).setCellValue("Ban Type");
+            header.createCell(6).setCellValue("Color");
+            header.createCell(7).setCellValue("Engine");
+            header.createCell(8).setCellValue("Mileage");
+            header.createCell(9).setCellValue("Transmission");
+            header.createCell(10).setCellValue("Gear");
+            header.createCell(11).setCellValue("New");
+            header.createCell(12).setCellValue("Seats");
+            header.createCell(13).setCellValue("Situation");
+            header.createCell(14).setCellValue("Market");
+        }
+        addData(sheet);
 
-            for (int i = currentPage; i < currentPage + 1; i++) {
-                Document doc = Jsoup.connect("https://turbo.az/autos/?page=" + i).get();
-                Elements adLinks = doc.select(".products-i__link");
-                for (Element adLink : adLinks) {
-                    String adUrl = adLink.attr("abs:href");
-                    Document doc1 = Jsoup.connect(adUrl).get();
-                    String name = doc1.select("h1.product-title").text();
-                    System.out.println("");
-                    System.out.println(name);
+        try (OutputStream os = new FileOutputStream("autos.xls")) {
+            wb.write(os);
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    }
+    private static void addData(Sheet sheet) throws IOException {
 
-                    String price = doc1.select(".product-price__i.product-price__i--bold").text();
-                    System.out.println("Price : " + price);
+        int currentPage = loadCurrentPage();
+        for (int i = currentPage; i < currentPage + 5; i++) {
+            System.out.println("Page "+ i + " page 1 parsed");
+            Document doc = Jsoup.connect("https://turbo.az/autos/?page=" + i).get();
+            Elements adLinks = doc.select(".tz-container .products .products-i__link");
+            for (Element adLink : adLinks) {
 
-                    Elements divs = doc1.select(".product-properties__column");
-                    Row newRow = sheet.createRow(sheet.getLastRowNum() + 1);
-                    newRow.createCell(0).setCellValue(price);
-                    int countCell = 1;
-                    for (Element div : divs){
-                        Elements elements = div.select(".product-properties__i ");
-                        for (Element element : elements) {}
-                        for (Element element : elements) {
-                            String elementLabel = element.select("label").text();
-                            String elementSpan = element.select("span").first().text();
-                            int m = newRow.getLastCellNum();
+                String adUrl = adLink.attr("abs:href");
+                Document doc1 = Jsoup.connect(adUrl).get();
 
-                            header.createCell(countCell).setCellValue(elementLabel);
-                            newRow.createCell(countCell++).setCellValue(elementSpan);
-                            System.out.println(elementLabel + " : " + elementSpan);
+                String price = doc1.select(".product-price__i.product-price__i--bold").text();
+
+                Elements divs = doc1.select(".product-properties__column");
+
+                Row newRow = sheet.createRow(sheet.getLastRowNum() + 1);
+                newRow.createCell(0).setCellValue(price);
+                for (Element div : divs) {
+
+                    Elements elements = div.select(".product-properties__i ");
+                    for (Element element : elements) {
+
+                        Element elementLabel = element.select("label").first();
+                        String attr = elementLabel.attr("for");
+                        String elementSpan = element.select("span").first().text();
+
+                        switch (attr){
+                            case "ad_region":
+                                newRow.createCell(1).setCellValue(elementSpan);
+                                break;
+                            case "ad_make_id":
+                                newRow.createCell(2).setCellValue(elementSpan);
+                                break;
+                            case "ad_model":
+                                newRow.createCell(3).setCellValue(elementSpan);
+                                break;
+                            case "ad_reg_year":
+                                newRow.createCell(4).setCellValue(elementSpan);
+                                break;
+                            case "ad_category":
+                                newRow.createCell(5).setCellValue(elementSpan);
+                                break;
+                            case "ad_color":
+                                newRow.createCell(6).setCellValue(elementSpan);
+                                break;
+                            case "ad_engine_volume":
+                                newRow.createCell(7).setCellValue(elementSpan);
+                                break;
+                            case "ad_mileage":
+                                newRow.createCell(8).setCellValue(elementSpan);
+                                break;
+                            case "ad_transmission":
+                                newRow.createCell(9).setCellValue(elementSpan);
+                                break;
+                            case "ad_gear":
+                                newRow.createCell(10).setCellValue(elementSpan);
+                                break;
+                            case "ad_new":
+                                newRow.createCell(11).setCellValue(elementSpan);
+                                break;
+                            case "ad_seats_count":
+                                newRow.createCell(12).setCellValue(elementSpan);
+                                break;
+                            case "ad_Vəziyyəti":
+                                newRow.createCell(13).setCellValue(elementSpan);
+                                break;
+                            case "ad_market":
+                                newRow.createCell(14).setCellValue(elementSpan);
+                                break;
+                            default:
                         }
                     }
                 }
             }
-            wb.write(os);
-            currentPage += 5;
+        }
+        currentPage += 5;
+        saveCurrentPageNumber(currentPage);
+        System.out.println("Current Page " + currentPage);
+    }
+    private static int loadCurrentPage() {
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileInputStream(CURRENT_PAGE_FILE));
+            String currentPageStr = prop.getProperty(CURRENT_PAGE);
+            if (currentPageStr != null) {
+                return Integer.parseInt(currentPageStr);
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    private static void saveCurrentPageNumber(int pageNumber) {
+        Properties prop = new Properties();
+        prop.setProperty(CURRENT_PAGE, String.valueOf(pageNumber));
+        try {
+            prop.store(new FileOutputStream(CURRENT_PAGE_FILE), null);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
-
-
-//package org.example;
-//
-//import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-//import org.apache.poi.ss.usermodel.Cell;
-//import org.apache.poi.ss.usermodel.Row;
-//import org.apache.poi.ss.usermodel.Sheet;
-//import org.apache.poi.ss.usermodel.Workbook;
-//import org.jsoup.Jsoup;
-//import org.jsoup.nodes.Document;
-//import org.jsoup.nodes.Element;
-//import org.jsoup.select.Elements;
-//
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.io.OutputStream;
-//
-//public class Main {
-//    private static int currentPage = 1;
-//    public static void main(String[] args) {
-//        int countRow = 1;
-//
-//        Workbook wb = new HSSFWorkbook();
-//        try(OutputStream os = new FileOutputStream("autos90.xls")) {
-//            Sheet sheet = wb.createSheet("Autos");
-//            Row header = sheet.createRow(0);
-//            for(int i=currentPage; i<currentPage + 3; i++) {
-//                Document doc = Jsoup.connect("https://turbo.az/autos/?page="+i).get();
-//                Elements adLinks = doc.select(".products-i__link");
-//
-//                for (Element adLink : adLinks) {
-//                    int countCell = 0;
-//                    String adUrl = adLink.attr("abs:href");
-//                    Document doc1 = Jsoup.connect(adUrl).get();
-//                    String name = doc1.select("h1.product-title ").text();
-//                    System.out.println("");
-//                    System.out.println(name);
-//
-//                    Row newRow = sheet.createRow(countRow++);
-//
-//                    Elements divs = doc1.select(".product-properties__column");
-//                    Cell newCell  = newRow.createCell(countCell);
-//                    Cell headerCell  = header.createCell(countCell);
-//                    headerCell.setCellValue("Price");
-//                    countCell++;
-//                    for(Element div: divs){
-//                        String price = doc1.select(".product-price__i.product-price__i--bold").text();
-//                        System.out.println("Price : "+price);
-//
-//                        newCell.setCellValue(price);
-//                        Elements elements = div.select(".product-properties__i ");
-//
-//                        for (Element element : elements) {
-//
-//                            String elementLabel = element.select("label").text();
-//                            Cell headerCell1  = header.createCell(countCell);
-//                            headerCell1.setCellValue(elementLabel);
-//
-//                            String elementSpan = element.select("span").first().text();
-//                            Cell newCell1  = newRow.createCell(countCell);
-//                            newCell1.setCellValue(elementSpan);
-//
-//                            System.out.println(elementLabel + " : " + elementSpan);
-//                            countCell++;
-//                        }
-//                    }
-//                    wb.write(os);
-//                }
-//            }
-//            currentPage += 5;
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
